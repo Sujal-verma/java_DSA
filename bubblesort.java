0809ed6561018b8882b73fb483ecc7536da3d2c1
Sujal verma <122211284+Sujal-verma@users.noreply.github.com>
2025-08-10 16:25:18 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,24 @@
+package sorting;
+public class bubblesort {
+    public static void sort(int arr[]){
+        for(int turn = 0;turn<arr.length-1;turn++){
+            for(int j= 0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void array(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,3,1,2};
+        sort(arr);
+        array(arr);
+    }
+}
