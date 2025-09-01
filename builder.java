d4358595e4b1b5cb4be44beda14bdb7717c2c331
Sujal verma <122211284+Sujal-verma@users.noreply.github.com>
2025-09-01 23:40:25 +0530

    Add files via upload

---
+++ builder.java
@@ -0,0 +1,11 @@
+package String;
+public class builder {
+    public static void main(String[] args) {
+        StringBuilder sv = new StringBuilder("");
+        Integer i = 4;
+        i.toString();
+        sv.append(i);
+        System.out.println(sv);
+    }
+
+}
